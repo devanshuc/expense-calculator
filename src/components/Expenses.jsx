8415a1d639a97de6d400d74323f5e014ea8d8301
devanshuc <66375026+devanshuc@users.noreply.github.com>
2023-05-22 11:38:31 +0530

    Some code structuring

--- src/components/Expenses.jsx
+++ src/components/Expenses.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
-import ExpenseItem from "./ExpenseItem";
 import "./Expenses.css";
 import ExpensesFilter from "./table/ExpensesFilter";
+import ExpenseList from "./ExpensesList";
 
 function Expenses(props) {
   const [filterYear, setFilterYear] = useState("2022");
@@ -14,23 +14,10 @@ function Expenses(props) {
     return item.date.getFullYear().toString() === filterYear;
   });
 
-  let expensesContent = <p>No Expenses Found</p>;
-
-  if (filteredExpenses.length > 0) {
-    expensesContent = filteredExpenses.map((expense) => (
-      <ExpenseItem
-        key={expense.id}
-        title={expense.title}
-        amount={expense.amount}
-        date={expense.date}
-      />
-    ));
-  }
-
   return (
     <div className="expenses">
       <ExpensesFilter selected={filterYear} getYear={theDropdownMenu} />
-      {expensesContent}
+      <ExpenseList items={filteredExpenses} />
     </div>
   );
 }
