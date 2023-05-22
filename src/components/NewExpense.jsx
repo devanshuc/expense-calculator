6500e8fbefdbcc4a0205fdcd1a1532f93c0d0beb
devanshuc <66375026+devanshuc@users.noreply.github.com>
2023-05-22 12:01:56 +0530

    Added 'Add New Expense' Button and related Functionalites

--- src/components/NewExpense.jsx
+++ src/components/NewExpense.jsx
@@ -1,19 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 import "./NewExpense.css";
 import ExpenseForm from "./ExpenseForm";
 
 const NewExpense = (props) => {
+  const [showForm, setShowForm] = useState(false);
   const saveExpenseDataHandler = (enteredExpenseData) => {
     const expenseData = {
       ...enteredExpenseData,
       id: Math.random().toString(),
     };
     props.onAddExpense(expenseData);
+    setShowForm(false);
+  };
+
+  const isShowingForm = () => {
+    setShowForm(true);
+  };
+
+  const isNotShowingForm = () => {
+    setShowForm(false);
   };
 
   return (
     <div className="new-expense">
-      <ExpenseForm onSaveExpenseData={saveExpenseDataHandler} />
+      {!showForm && <button onClick={isShowingForm}>Add New Expense</button>}
+      {showForm && (
+        <ExpenseForm
+          onSaveExpenseData={saveExpenseDataHandler}
+          onCancel={isNotShowingForm}
+        />
+      )}
     </div>
   );
 };
